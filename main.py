902737c52a60f6a21c113d47047c84aab75e1bc2
lethosor <lethosor@gmail.com>
2024-07-12 18:19:54 -0400

    Add initial implementation

---
+++ main.py
@@ -0,0 +1,42 @@
+import argparse
+import datetime
+import json
+import os
+import threading
+
+import bottle
+
+
+class Logger:
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+        self.lock = threading.Lock()
+
+    def log(self, message):
+        with self.lock:
+            with open(os.path.join(self.data_dir, self._get_current_filename()), 'a') as f:
+                f.write(message.strip('\n') + '\n')
+
+    def _get_current_filename(self):
+        return datetime.datetime.utcnow().strftime('%Y%m%d') + '.json.log'
+
+logger: Logger = None
+
+@bottle.post('/pub')
+def handle_publish():
+    body = json.dumps(bottle.request.json)
+    if len(body) > 1024:
+        raise ValueError(f"Body too large: {body}")
+    logger.log(body)
+    return '[]'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-o', '--host', default='localhost')
+parser.add_argument('-p', '--port', type=int, default=8035)
+parser.add_argument('-d', '--data-dir', default='data')
+args = parser.parse_args()
+
+os.makedirs(args.data_dir, exist_ok=True)
+logger = Logger(args.data_dir)
+
+bottle.run(host=args.host, port=args.port)
